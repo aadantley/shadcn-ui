bb5aacc9bbcb82827bb63bf65438fcd48d191b80
Aaron Dantley <aadantley@gmail.com>
2024-12-11 13:13:01 -0500

    adding sidebar component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import MyAccordion from "@/Shadcn/MyAccordion";
 
 export default function Home() {
   return (
-    <div className="flex p-3 w-50">
+    <>
       <MyAccordion />
-    </div>
+    </>
   );
 }
